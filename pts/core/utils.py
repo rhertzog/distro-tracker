9ed5562d2f4c54177edb25e551e3d646c89d95f4
Marko Lalic <marko.lalic@gmail.com>
2013-06-11 21:46:06 +0200

    Add utility function to extract email from header.

--- pts/core/utils.py
+++ pts/core/utils.py
@@ -10,3 +10,17 @@ def get_or_none(model, **kwargs):
         return model.objects.get(**kwargs)
     except model.DoesNotExist:
         return None
+
+
+def extract_email_address_from_header(header):
+    """
+    Extracts the email address from the From email header.
+
+    >>> str(extract_email_address_from_header('Real Name <foo@domain.com>'))
+    'foo@domain.com'
+    >>> str(extract_email_address_from_header('foo@domain.com'))
+    'foo@domain.com'
+    """
+    from email.utils import parseaddr
+    real_name, from_address = parseaddr(header)
+    return from_address
