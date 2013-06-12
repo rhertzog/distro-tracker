a3885808ddb00bacd25cd78eb11ad5192481fa89
Marko Lalic <marko.lalic@gmail.com>
2013-06-12 03:28:00 +0200

    Implement basic email dispatch.

--- pts/core/utils.py
+++ pts/core/utils.py
@@ -24,3 +24,74 @@ def extract_email_address_from_header(header):
     from email.utils import parseaddr
     real_name, from_address = parseaddr(header)
     return from_address
+
+
+class DuplicateDict(object):
+    """
+    A container class which stores key, value pairs where the key does not
+    have to be unique.
+
+    When accessing the container, the value returned for a given key is defined
+    as the first value which was put in the container for that key.
+
+    >>> d = DuplicateDict()
+    >>> d.add('key1', 'value1')
+    >>> d.add('key2', 'value2')
+    >>> d.add('key1', 'value3')
+    >>> str(d.get('key1'))
+    'value1'
+    >>> str(d.get('key2'))
+    'value2'
+
+    When iterating through all the items in the container, all key, value pairs
+    are returned in an undefined order.
+
+    >>> l = list((str(key), str(value)) for key, value in d.items())
+    >>> len(l)
+    3
+    >>> ('key1', 'value1') in l
+    True
+    >>> ('key1', 'value3') in l
+    True
+    >>> ('key2', 'value2') in l
+    True
+    """
+    def __init__(self):
+        self._items = {}
+
+    def items(self):
+        return (
+            (key, value)
+            for key, values in self._items.items()
+            for value in values
+        )
+
+    def __iter__(self):
+        def wrap():
+            # The key is yielded as many times as there are items in the dict
+            for key, values in self._items.items():
+                for _ in values:
+                    yield key
+        return wrap()
+
+    def get(self, key, default=None):
+        if key not in self._items:
+            return default
+        else:
+            return self._items[key][0]
+
+    def __getitem__(self, key):
+        return self.get(key)
+
+    def add(self, key, value):
+        if key not in self._items:
+            self._items[key] = []
+        self._items[key].append(value)
+
+    def set(self, key, value):
+        if key not in self._items:
+            self._items[key] = []
+        if len(self._items[key]) == 0:
+            self._items[key].append(value)
+        else:
+            self._items[key][0] = value
--- pts/dispatch/process.py
+++ pts/dispatch/process.py
@@ -1,5 +1,102 @@
 from __future__ import unicode_literals
+from django.core.mail import EmailMessage
+from django.core.mail import get_connection
+
+from email import message_from_string
+from email.iterators import typed_subpart_iterator
+
+from core.utils import extract_email_address_from_header
+from core.utils import DuplicateDict
+from core.utils import get_or_none
+
+from core.models import Package
+from django.conf import settings
+OWNER_EMAIL_ADDRESS = getattr(settings, 'OWNER_EMAIL_ADDRESS')
+CONTROL_EMAIL_ADDRESS = getattr(settings, 'CONTROL_EMAIL_ADDRESS')
 
 
-def process(message):
-    pass
+def process(message, local_part=None):
+    """
+    Handles the dispatching of received messages.
+    """
+    msg = message_from_string(message)
+
+    if local_part is None:
+        from_email = extract_email_address_from_header(msg['To'])
+        local_part = from_email.split('@')[0]
+
+    package_name = local_part
+
+    send_to_subscribers(msg, package_name)
+
+
+def prepare_message(received_message, package_name, to_email):
+    headers = extract_headers(received_message)
+    add_new_headers(headers, package_name)
+    content = extract_content(received_message)
+    message = EmailMessage(
+        subject=headers['Subject'],
+        from_email=headers['From'],
+        headers=headers,
+        body=content,
+        to=[to_email]
+    )
+
+    return message
+
+
+def add_new_headers(headers, package_name):
+    new_headers = [
+        ('X-Loop', '{package}@packages.qa.debian.org'.format(
+            package=package_name)),
+        ('X-PTS-Package', package_name),
+        ('X-Debian-Package', package_name),
+        ('X-Debian', 'PTS'),
+        ('Precedence', 'list'),
+        ('List-Unsubscribe',
+            '<mailto:{control_email}?body=unsubscribe%20{package}>'.format(
+                control_email=CONTROL_EMAIL_ADDRESS,
+                package=package_name)),
+    ]
+    for header_name, header_value in new_headers:
+        headers.add(header_name, header_value)
+
+
+def extract_headers(received_message):
+    headers = DuplicateDict()
+    for key, value in received_message.items():
+        if key.lower() == 'to':
+            continue
+        headers.add(key, value)
+    return headers
+
+
+def extract_content(msg):
+    plain_text_part = next(typed_subpart_iterator(msg, 'text', 'plain'), None)
+    if not plain_text_part:
+        # There is no plain text in the email
+        return ''
+
+    # Decode the plain text into a unicode string
+    charset = plain_text_part.get_content_charset('ascii')
+    try:
+        text = plain_text_part.get_payload(decode=True).decode(charset)
+    except UnicodeDecodeError:
+        return ''
+    return text
+
+
+def send_to_subscribers(received_message, package_name):
+    package = get_or_none(Package, name=package_name)
+    if not package:
+        return
+    # Build a list of all messages to be sent
+    messages_to_send = [
+        prepare_message(received_message,
+                        package_name,
+                        subscriber.email)
+        for subscriber in package.subscriptions.all()
+    ]
+    # Send all messages over a single SMTP connection
+    connection = get_connection()
+    connection.send_messages(messages_to_send)
