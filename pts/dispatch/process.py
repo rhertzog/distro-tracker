045f90cf762f80659c6aa696c8e3d9972ed4ccaf
Marko Lalic <marko.lalic@gmail.com>
2013-06-13 03:18:04 +0200

    Fix message dispatch to maintain header order.
    The dispatch now uses the new CustomEmailMessage class instead of
    Django's EmailMessage class.

--- pts/dispatch/process.py
+++ pts/dispatch/process.py
@@ -1,13 +1,12 @@
 from __future__ import unicode_literals
-from django.core.mail import EmailMessage
 from django.core.mail import get_connection
 
 from email import message_from_string
-from email.iterators import typed_subpart_iterator
 
 from core.utils import extract_email_address_from_header
-from core.utils import DuplicateDict
 from core.utils import get_or_none
+
+from dispatch.custom_email_message import CustomEmailMessage
 
 from core.models import Package
 from django.conf import settings
@@ -27,25 +26,16 @@ def process(message, local_part=None):
 
     package_name = local_part
 
+    add_new_headers(msg, package_name)
     send_to_subscribers(msg, package_name)
 
 
-def prepare_message(received_message, package_name, to_email):
-    headers = extract_headers(received_message)
-    add_new_headers(headers, package_name)
-    content = extract_content(received_message)
-    message = EmailMessage(
-        subject=headers['Subject'],
-        from_email=headers['From'],
-        headers=headers,
-        body=content,
-        to=[to_email]
-    )
-
+def prepare_message(received_message, to_email):
+    message = CustomEmailMessage(msg=received_message, to=[to_email])
     return message
 
 
-def add_new_headers(headers, package_name):
+def add_new_headers(received_message, package_name):
     new_headers = [
         ('X-Loop', '{package}@packages.qa.debian.org'.format(
             package=package_name)),
@@ -59,31 +49,7 @@ def add_new_headers(headers, package_name):
                 package=package_name)),
     ]
     for header_name, header_value in new_headers:
-        headers.add(header_name, header_value)
-
-
-def extract_headers(received_message):
-    headers = DuplicateDict()
-    for key, value in received_message.items():
-        if key.lower() == 'to':
-            continue
-        headers.add(key, value)
-    return headers
-
-
-def extract_content(msg):
-    plain_text_part = next(typed_subpart_iterator(msg, 'text', 'plain'), None)
-    if not plain_text_part:
-        # There is no plain text in the email
-        return ''
-
-    # Decode the plain text into a unicode string
-    charset = plain_text_part.get_content_charset('ascii')
-    try:
-        text = plain_text_part.get_payload(decode=True).decode(charset)
-    except UnicodeDecodeError:
-        return ''
-    return text
+        received_message[header_name] = header_value
 
 
 def send_to_subscribers(received_message, package_name):
@@ -92,9 +58,7 @@ def send_to_subscribers(received_message, package_name):
         return
     # Build a list of all messages to be sent
     messages_to_send = [
-        prepare_message(received_message,
-                        package_name,
-                        subscriber.email)
+        prepare_message(received_message, subscriber.email)
         for subscriber in package.subscriptions.all()
     ]
     # Send all messages over a single SMTP connection
