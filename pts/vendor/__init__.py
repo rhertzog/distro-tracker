c60340a347ef61198f3ec75d089c3dc0d90117bc
Marko Lalic <marko.lalic@gmail.com>
2013-06-27 23:00:38 +0200

    Move documentation regarding vendor-specific rules.
    It is now found in the ``vendor`` package-level documentation.
    All callables which vendors can implement are listed there.

--- pts/vendor/__init__.py
+++ pts/vendor/__init__.py
@@ -8,3 +8,26 @@
 # Tracking System, including this file, may be copied, modified, propagated, or
 # distributed except according to the terms contained in the LICENSE file.
 
+"""An app which enables the PTS to access vendor-specific functionality.
+
+Vendors can define their own specific set of rules for mail dispatch
+which the PTS can plug in to appropriate functions.
+
+Each rule should be implemented as a callable which takes a defined set of
+arguments with a predefined name. The supported callables are:
+
+- ``get_keyword(local_part, message)`` - takes a local_part of the email
+  address to which a message was sent and an email Message object.
+  Should return a keyword which matches the message or None if it does not
+  match any keyword.
+
+- ``add_new_headers(received_message, package_name, keyword)`` -
+  takes an email Message object, the name of the package and the keyword
+  of the message.
+  Should return a list of two-tuples (header_name, header_value) of headers
+  which should be added to the response message.
+
+- ``approve_default_message(message)`` - takes an email Message object.
+  Should return a Boolean indicating whether this message should still be
+  forwarded to subscribers which are subscribed to the default keyword.
+"""
