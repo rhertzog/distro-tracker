1773544e45f57662c6d9fe76afe537a1fd154359
Marko Lalic <marko.lalic@gmail.com>
2013-06-30 00:32:56 +0200

    Refactor the pts_render_to_string function.
    It now uses the same extra context variables as the custom
    request context processor, reducing duplication and making sure the
    same context is always available to both.

--- pts/core/utils/__init__.py
+++ pts/core/utils/__init__.py
@@ -9,9 +9,7 @@
 # distributed except according to the terms contained in the LICENSE file.
 
 from __future__ import unicode_literals
-from django.conf import settings
 from django.template.loader import render_to_string
-from django.contrib.sites.models import Site
 
 from .email_messages import extract_email_address_from_header
 from .email_messages import get_decoded_message_payload
@@ -38,14 +36,10 @@ def pts_render_to_string(template_name, context=None):
     work when using a RequestContext, wheras this function can be called
     independently from any HTTP request.
     """
+    from pts.core import context_processors
     if context is None:
         context = {}
-    extra_context = {
-        'PTS_VENDOR_NAME': settings.PTS_VENDOR_NAME,
-        'PTS_CONTACT_EMAIL': settings.PTS_CONTACT_EMAIL,
-        'PTS_CONTROL_EMAIL': settings.PTS_CONTROL_EMAIL,
-        'PTS_SITE_DOMAIN': Site.objects.get_current(),
-    }
+    extra_context = context_processors.PTS_EXTRAS
     context.update(extra_context)
 
     return render_to_string(template_name, context)
