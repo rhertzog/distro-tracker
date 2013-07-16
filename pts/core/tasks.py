a5da830fde583a9265f13af06b6454b20fd5a756
Marko Lalic <marko.lalic@gmail.com>
2013-07-16 09:11:54 +0200

    Add a string representation of Event class instances.

--- pts/core/tasks.py
+++ pts/core/tasks.py
@@ -157,6 +157,12 @@ class Event(object):
         self.name = name
         self.arguments = arguments
 
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
 
 class TaskDAG(DAG):
     """
