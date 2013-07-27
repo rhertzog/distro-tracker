166974f9132d44e1d83b4b5506eada1ae89dadf8
Marko Lalic <marko.lalic@gmail.com>
2013-07-28 00:03:49 +0200

    Implement Debian-specific bug stats retrieval tasks.
    The tasks retrieve Debian bug stats by using UDD resources.

--- pts/vendor/debian/tasks.py
+++ pts/vendor/debian/tasks.py
@@ -18,10 +18,17 @@ from django.conf import settings
 
 from pts.core.tasks import BaseTask
 from pts.core.models import ContributorEmail
+from pts.core.models import PackageBugStats
+from pts.core.models import BinaryPackageBugStats
+from pts.core.models import PackageName
+from pts.core.models import BinaryPackageName
 from pts.core.utils.http import HttpCache
 from .models import DebianContributor
 import re
 from debian import deb822
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 class RetrieveDebianMaintainersTask(BaseTask):
@@ -138,3 +145,136 @@ class RetrieveLowThresholdNmuTask(BaseTask):
 
                 contributor.agree_with_low_threshold_nmu = True
                 contributor.save()
+
+
+class UpdatePackageBugStats(BaseTask):
+    bug_categories = (
+        'rc',
+        'normal',
+        'wishlist',
+        'fixed',
+        'patch',
+    )
+
+    def __init__(self, force_update=False, *args, **kwargs):
+        super(UpdatePackageBugStats, self).__init__(*args, **kwargs)
+        self.force_update = force_update
+        self.cache = HttpCache(settings.PTS_CACHE_DIRECTORY)
+
+    def _get_response(self, url):
+        """
+        Helper method which returns either the resource at the given URL or
+        ``None``, depending on the :attr:`UpdatePackageBugStats.force_update`
+        flag and cache status.
+
+        :param url: The URL of the resource to retrieve.
+
+        :returns None: If the resource found at the given URL is still fresh in
+            the cache and the :attr:`UpdatePackageBugStats.force_update` was
+            set to ``False``
+        :returns requests.Response: If the cached resource has expired or
+            :attr:`UpdatePackageBugStats.force_update` was set to ``True``
+        """
+        if not self.force_update and not self.cache.is_expired(url):
+            return
+        response, updated = self.cache.update(url, force=self.force_update)
+        response.raise_for_status()
+        if not updated:
+            return
+        return response
+
+    def update_source_and_pseudo_bugs(self):
+        """
+        Performs the update of bug statistics for source and pseudo packages.
+        """
+        url = 'http://udd.debian.org/cgi-bin/ddpo-bugs.cgi'
+        response = self._get_response(url)
+        if not response:
+            return
+
+        # Each line in the response should be bug stats for a single package
+        bug_stats = {}
+        for line in response.iter_lines():
+            line = line.decode('utf-8')
+            package_name, bug_counts = line.split(':', 1)
+            # Merged counts are in parentheses so remove those before splitting
+            # the numbers
+            bug_counts = re.sub(r'[()]', ' ', bug_counts).split()
+            try:
+                bug_counts = [int(count) for count in bug_counts]
+            except ValueError:
+                logger.exception(
+                    'Failed to parse bug information for {pkg}: {cnts}'.format(
+                        pkg=package_name, cnts=bug_counts))
+                continue
+
+            # Match the extracted counts with category names
+            bug_stats[package_name] = [
+                {
+                    'category_name': category_name,
+                    'bug_count': bug_count,
+                    'merged_count': merged_count,
+                }
+                for category_name, (bug_count, merged_count) in zip(
+                    self.bug_categories, zip(bug_counts[::2], bug_counts[1::2]))
+            ]
+
+        with transaction.commit_on_success():
+            # Clear previous stats
+            PackageBugStats.objects.all().delete()
+            packages = PackageName.objects.filter(name__in=bug_stats.keys())
+            # Create new stats in a single query
+            stats = [
+                PackageBugStats(package=package, stats=bug_stats[package.name])
+                for package in packages
+            ]
+            PackageBugStats.objects.bulk_create(stats)
+
+    def update_binary_bugs(self):
+        """
+        Performs the update of bug statistics for binary packages.
+        """
+        url = 'http://udd.debian.org/cgi-bin/bugs-binpkgs-pts.cgi'
+        response = self._get_response(url)
+        if not response:
+            return
+
+        # Extract known binary package bug stats: each line is a separate pkg
+        bug_stats = {}
+        for line in response.iter_lines():
+            line = line.decode('utf-8')
+            package_name, bug_counts = line.split(None, 1)
+            bug_counts = bug_counts.split()
+            try:
+                bug_counts = [int(count) for count in bug_counts]
+            except ValueError:
+                logger.exception(
+                    'Failed to parse bug information for {pkg}: {cnts}'.format(
+                        pkg=package_name, cnts=bug_counts))
+                continue
+
+            bug_stats[package_name] = [
+                {
+                    'category_name': category_name,
+                    'bug_count': bug_count,
+                }
+                for category_name, bug_count in zip(
+                    self.bug_categories, bug_counts)
+            ]
+
+        with transaction.commit_on_success():
+            # Clear previous stats
+            BinaryPackageBugStats.objects.all().delete()
+            packages = BinaryPackageName.objects.filter(name__in=bug_stats.keys())
+            # Create new stats in a single query
+            stats = [
+                BinaryPackageBugStats(package=package, stats=bug_stats[package.name])
+                for package in packages
+            ]
+            BinaryPackageBugStats.objects.bulk_create(stats)
+
+    def execute(self):
+        # Stats for source and pseudo packages is retrieved from a different
+        # resource (with a different structure) than stats for binary packages.
+        self.update_source_and_pseudo_bugs()
+        self.update_binary_bugs()
