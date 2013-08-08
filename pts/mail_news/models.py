85b68cba0caa4df6faa0a32b9fdd07f33eb791e4
Marko Lalic <marko.lalic@gmail.com>
2013-08-08 19:35:08 +0200

    Remove the old apps which have been moved into pts.mail.
    Removing apps:
    - pts.dispatch
    - pts.mail_news
    - pts.control
    Since they are now integrated into the pts.mail app.

--- pts/mail_news/models.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2013 The Debian Package Tracking System Developers
-# See the COPYRIGHT file at the top-level directory of this distribution and
-# at http://deb.li/ptsauthors
-#
-# This file is part of the Package Tracking System. It is subject to the
-# license terms in the LICENSE file found in the top-level directory of
-# this distribution and at http://deb.li/ptslicense. No part of the Package
-# Tracking System, including this file, may be copied, modified, propagated, or
-# distributed except according to the terms contained in the LICENSE file.
