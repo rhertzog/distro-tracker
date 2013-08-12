005087472980c023d4590e65e7d9264f3423c470
Marko Lalic <marko.lalic@gmail.com>
2013-08-12 20:47:46 +0200

    Implement Standards-Version warnings as "action needed" items.
    If a package's Standards-Version is older than the version of the
    debian-policy package, an ActionItem is created.
    
    This functionality is extractd to a separate app in order to allow
    vendors to activate or deactivate it as they see fit.

---
+++ pts/stdver_warnings/pts_tasks.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2013 The Debian Package Tracking System Developers
+# See the COPYRIGHT file at the top-level directory of this distribution and
+# at http://deb.li/ptsauthors
+#
+# This file is part of the Package Tracking System. It is subject to the
+# license terms in the LICENSE file found in the top-level directory of
+# this distribution and at http://deb.li/ptslicense. No part of the Package
+# Tracking System, including this file, may be copied, modified, propagated, or
+# distributed except according to the terms contained in the LICENSE file.
+"""
+PTS tasks for the :mod:`pts.stdver_warnings` app.
+"""
+
+from __future__ import unicode_literals
+from pts.core.tasks import BaseTask
+from pts.core.utils import get_or_none
+from pts.core.models import SourcePackageName
+from pts.core.models import ActionItem, ActionItemType
+
+
+class UpdateStandardsVersionWarnings(BaseTask):
+    """
+    The task updates warnings for packages which have an outdated
+    Standards-Version.
+    """
+    DEPENDS_ON_EVENTS = (
+        'new-source-package-version',
+    )
+
+    ACTION_ITEM_TYPE = 'debian-std-ver-outdated'
+
+    ITEM_DESCRIPTION = "Standards version of the package is outdated."
+
+    def __init__(self, force_update=False, *args, **kwargs):
+        super(UpdateStandardsVersionWarnings, self).__init__(*args, **kwargs)
+        self.force_update = force_update
+        self.action_type, _ = ActionItemType.objects.get_or_create(
+            type_name=self.ACTION_ITEM_TYPE)
+
+    def set_parameters(self, parameters):
+        if 'force_update' in parameters:
+            self.force_update = parameters['force_update']
+
+    def get_packages_from_events(self):
+        """
+        :returns: A list of :class:`pts.core.models.SourcePackageName`
+            instances which are found from all raised events.
+        """
+        package_pks = [
+            event.arguments['pk']
+            for event in self.get_all_events()
+        ]
+        qs = SourcePackageName.objects.filter(source_package_versions__pk__in=package_pks)
+        qs.prefetch_related('action_items')
+
+        return qs
+
+    def get_policy_version(self):
+        """
+        :returns: The latest version of the ``debian-policy`` package.
+        """
+        debian_policy = get_or_none(SourcePackageName, name='debian-policy')
+        if not debian_policy:
+            return
+        policy_version = debian_policy.main_version.version
+        # Minor patch level should be disregarded for the comparison
+        policy_version, _ = policy_version.rsplit('.', 1)
+
+        return policy_version
+
+    def get_action_item(self, package):
+        """
+        :returns: A :class:`pts.core.models.ActionItem` instance of the
+            :attr:`ACTION_ITEM_TYPE` for the given package.
+            ``None`` if the package previously did not have an ActionItem
+            of that type.
+        """
+        return next((
+            item
+            for item in package.action_items.all()
+            if item.item_type.type_name == self.ACTION_ITEM_TYPE),
+            None)
+
+    def create_action_item(self, package, policy_version):
+        """
+        Creates a :class:`pts.core.models.ActionItem` instance if the
+        Standards-Version of the given package is outdated when compared to the
+        given policy version.
+        """
+        if not package.main_version:
+            return
+        # Get the old action item entry
+        action_item = self.get_action_item(package)
+        standards_version = package.main_version.standards_version
+        if standards_version.startswith(policy_version):
+            # The std-ver of the package is up to date.
+            # Remove any possibly existing action item.
+            if action_item is not None:
+                action_item.delete()
+            return
+
+        major_policy_version_number, _ = policy_version.split('.', 1)
+        severely_outdated = not standards_version.startswith(
+            major_policy_version_number)
+
+        if action_item is None:
+            action_item = ActionItem(
+                package=package,
+                item_type=self.action_type,
+                full_description_template='stdver_warnings/standards-version-action-item.html')
+
+        # Remove the minor patch level from the package's Std-Ver, if it has it
+        if standards_version.count('.') == 3:
+            standards_version, _ = standards_version.rsplit('.', 1)
+
+        action_item.short_description = self.ITEM_DESCRIPTION
+        action_item.extra_data = {
+            'lastsv': policy_version,
+            'standards_version': standards_version,
+            'severely_outdated': severely_outdated,
+        }
+        action_item.save()
+
+    def execute(self):
+        # Get the current policy version
+        policy_version = self.get_policy_version()
+        if policy_version is None:
+            # Nothing to do if there is no ``debian-policy``
+            return
+
+        if self.is_initial_task():
+            # If the task is directly ran, update all packages
+            packages = SourcePackageName.objects.all()
+            packages.fetch_related('action_items')
+        else:
+            # If the task is ran as part of a job, get the packages from raised
+            # events
+            packages = self.get_packages_from_events()
+
+        for package in packages:
+            self.create_action_item(package, policy_version)
---
+++ pts/stdver_warnings/tests.py
@@ -0,0 +1,270 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2013 The Debian Package Tracking System Developers
+# See the COPYRIGHT file at the top-level directory of this distribution and
+# at http://deb.li/ptsauthors
+#
+# This file is part of the Package Tracking System. It is subject to the
+# license terms in the LICENSE file found in the top-level directory of
+# this distribution and at http://deb.li/ptslicense. No part of the Package
+# Tracking System, including this file, may be copied, modified, propagated, or
+# distributed except according to the terms contained in the LICENSE file.
+"""
+Tests for the :mod:`pts.stdver_warnings` app.
+"""
+
+from __future__ import unicode_literals
+from django.test import TestCase
+from django.utils.six.moves import mock
+from pts.stdver_warnings.pts_tasks import UpdateStandardsVersionWarnings
+from pts.core.tasks import Event, Job, JobState
+from pts.core.models import SourcePackageName
+from pts.core.models import SourcePackage
+from pts.core.models import ActionItem, ActionItemType
+
+
+class StandardsVersionActionItemTests(TestCase):
+    """
+    Tests for the
+    :class:`pts.stdver_warnings.pts_tasks.UpdateStandardsVersionWarnings`
+    task.
+    """
+    def setUp(self):
+        self.package_name = SourcePackageName.objects.create(name='dummy-package')
+        self.package = SourcePackage.objects.create(
+            source_package_name=self.package_name, version='1.0.0')
+
+        self.default_policy_version = '3.9.4.0'
+        self.debian_policy_name = SourcePackageName.objects.create(
+            name='debian-policy')
+        self.debian_policy = SourcePackage.objects.create(
+            source_package_name=self.debian_policy_name,
+            version=self.default_policy_version)
+
+        self.job_state = mock.create_autospec(JobState)
+        self.job_state.events_for_task.return_value = []
+        self.job = mock.create_autospec(Job)
+        self.job.job_state = self.job_state
+        self.task = UpdateStandardsVersionWarnings()
+        self.task.job = self.job
+
+    def add_mock_event(self, name, arguments):
+        """
+        Helper method adding mock events which the news generation task will
+        see when it runs.
+        """
+        self.job_state.events_for_task.return_value.append(
+            Event(name=name, arguments=arguments)
+        )
+
+    def run_task(self, initial_task=False):
+        """
+        Initiates the task run.
+
+        :param initial_task: An optional flag which if ``True`` means that the
+            task should be ran as if it were directly passed to the
+            :func:`pts.core.tasks.run_task` function.
+        :type initial_task: Boolean
+        """
+        if initial_task:
+            self.job_state.events_for_task.return_value = []
+        else:
+            # If it is not the initial task, add a dummy task to make it look
+            # like that.
+            self.job_state.processed_tasks = ['sometask']
+
+        self.task.execute()
+
+    def set_debian_policy_version(self, policy_version):
+        """
+        Set the version of the debian-policy package to the given version.
+        """
+        self.debian_policy.version = policy_version
+        self.debian_policy.save()
+
+    def get_action_type(self):
+        """
+        Returns the :class:`pts.core.models.ActionItemType` for
+        Standards-Version warnings.
+        """
+        return ActionItemType.objects.get_or_create(
+            type_name=UpdateStandardsVersionWarnings.ACTION_ITEM_TYPE)[0]
+
+    def add_mock_new_source_version_event(self, package):
+        """
+        Helper method adding mock 'new-source-package-version' events where
+        the newly created source package should be the one given in the
+        ``package`` parameter.
+        """
+        self.add_mock_event('new-source-package-version', {
+            'pk': package.pk,
+        })
+
+    def test_action_item_outdated_policy(self):
+        """
+        Tests that an action item is created when the package's standards
+        version is outdated.
+        """
+        policy_version = '3.9.4.0'
+        self.set_debian_policy_version(policy_version)
+        # Set the std-ver below the policy version
+        self.package.standards_version = '3.9.3'
+        self.package.save()
+        # Sanity check: no action item
+        self.assertEqual(0, ActionItem.objects.count())
+        self.add_mock_new_source_version_event(self.package)
+
+        self.run_task()
+
+        # Action item created.
+        self.assertEqual(1, ActionItem.objects.count())
+        # Correct type?
+        item = ActionItem.objects.all()[0]
+        self.assertEqual(
+            item.item_type.type_name,
+            self.get_action_type().type_name)
+        # Contains the correct package standards version in extra data
+        self.assertEqual(
+            self.package.standards_version,
+            item.extra_data['standards_version'])
+        self.assertFalse(item.extra_data['severely_outdated'])
+
+    def test_action_item_severely_outdated_policy(self):
+        """
+        Tests that an action item is created when the package's standards
+        version is severely outdated (major version number differs from the
+        major version number of debian-policy).
+        """
+        policy_version = '3.9.4.0'
+        self.set_debian_policy_version(policy_version)
+        # Set the std-ver below the policy version
+        self.package.standards_version = '2.9.3'
+        self.package.save()
+        # Sanity check: no action item
+        self.assertEqual(0, ActionItem.objects.count())
+        self.add_mock_new_source_version_event(self.package)
+
+        self.run_task()
+
+        # Action item created.
+        self.assertEqual(1, ActionItem.objects.count())
+        # Contains the correct package standards version in extra data
+        item = ActionItem.objects.all()[0]
+        self.assertTrue(item.extra_data['severely_outdated'])
+
+    def test_no_action_item_policy_up_to_date(self):
+        """
+        Tests that no action item is created when the package's
+        Standards-Version is up to date.
+        """
+        policy_version = '3.9.4.0'
+        self.set_debian_policy_version(policy_version)
+        # Set the std-ver to be equal to the policy version.
+        self.package.standards_version = '3.9.4'
+        self.package.save()
+        # Sanity check: no action item
+        self.assertEqual(0, ActionItem.objects.count())
+        self.add_mock_new_source_version_event(self.package)
+
+        self.run_task()
+
+        # Still no action items.
+        self.assertEqual(0, ActionItem.objects.count())
+
+    def test_action_item_policy_outdated_full_version(self):
+        """
+        Tests that an action item is created when the package's standards
+        version is outdated and set by giving all 4 version numbers.
+        """
+        policy_version = '3.9.4.0'
+        self.set_debian_policy_version(policy_version)
+        # Set the std-ver below the policy version
+        self.package.standards_version = '3.9.3.1'
+        self.package.save()
+        # Sanity check: no action item
+        self.assertEqual(0, ActionItem.objects.count())
+        self.add_mock_new_source_version_event(self.package)
+
+        self.run_task()
+
+        # Action item created.
+        self.assertEqual(1, ActionItem.objects.count())
+        item = ActionItem.objects.all()[0]
+        self.assertEqual(
+            item.item_type.type_name,
+            self.get_action_type().type_name)
+
+    def test_no_action_item_policy_up_to_date_full_version(self):
+        """
+        Tests that no action item is created when the package's
+        Standards-Version is up to date and set by giving all 4 version
+        numbers.
+        """
+        policy_version = '3.9.4.0'
+        self.set_debian_policy_version(policy_version)
+        # Set the std-ver to be equal to the policy version.
+        self.package.standards_version = policy_version
+        self.package.save()
+        # Sanity check: no action item
+        self.assertEqual(0, ActionItem.objects.count())
+        self.add_mock_new_source_version_event(self.package)
+
+        self.run_task()
+
+        # Still no action items.
+        self.assertEqual(0, ActionItem.objects.count())
+
+    def test_action_item_removed(self):
+        """
+        Tests that an existing action item is removed when there is a new
+        package version with a non-outdated Std-Ver.
+        """
+        policy_version = '3.9.4.0'
+        self.set_debian_policy_version(policy_version)
+        action_type = self.get_action_type()
+        ActionItem.objects.create(
+            package=self.package_name,
+            item_type=action_type,
+            short_description="Desc")
+        self.package.standards_version = '3.9.3'
+        self.package.save()
+        # Create a new package with a higher Std-Ver
+        new_package = SourcePackage.objects.create(
+            source_package_name=self.package_name,
+            version='4.0.0',
+            standards_version='3.9.4.0')
+        self.add_mock_new_source_version_event(new_package)
+
+        self.run_task()
+
+        # The action item has been removed.
+        self.assertEqual(0, self.package_name.action_items.count())
+
+    def test_action_item_updated(self):
+        """
+        Tests that an existing action item is updated when there is a new
+        package version which still has an outdated Std-Ver.
+        """
+        policy_version = '3.9.4.0'
+        self.set_debian_policy_version(policy_version)
+        action_type = self.get_action_type()
+        ActionItem.objects.create(
+            package=self.package_name,
+            item_type=action_type,
+            short_description="Desc")
+        self.package.standards_version = '3.9.2'
+        self.package.save()
+        # Create a new package with a higher Std-Ver
+        new_package = SourcePackage.objects.create(
+            source_package_name=self.package_name,
+            version='4.0.0',
+            standards_version='3.9.3.0')
+        self.add_mock_new_source_version_event(new_package)
+
+        self.run_task()
+
+        # Still only one action item
+        self.assertEqual(1, self.package_name.action_items.count())
+        # The standards version in the extra data has been updated
+        item = self.package_name.action_items.all()[0]
+        self.assertEqual('3.9.3', item.extra_data['standards_version'])
