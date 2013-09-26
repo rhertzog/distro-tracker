bb3a58540f813cdb178398222d02af48b495faae
Raphaël Hertzog <hertzog@debian.org>
2013-09-26 16:00:09 +0200

    Update copyright headers to refer to the Distro Tracker Developers
    Also use updated short URLs for the COPYRIGHT and LICENSE files:
    http://deb.li/DTAuthors & http://deb.li/DTLicense
    
    And update naming in parameters for generated documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -41,8 +41,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'Package Tracking System'
-copyright = u'2013, Debian Package Tracking System Developers'
+project = u'Distro Tracker'
+copyright = u'2013, The Distro Tracker Developers'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -168,7 +168,7 @@ html_static_path = ['_static']
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'PackageTrackingSystemdoc'
+htmlhelp_basename = 'DistroTrackerDoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -187,8 +187,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'PackageTrackingSystem.tex', u'Package Tracking System Documentation',
-   u'Debian Package Tracking System Developers', 'manual'),
+  ('index', 'DistroTracker.tex', u'Distro Tracker Documentation',
+   u'The Distro Tracker Developers', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -217,8 +217,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'packagetrackingsystem', u'Package Tracking System Documentation',
-     [u'Debian Package Tracking System Developers'], 1)
+    ('index', 'distrotracker', u'Distro Tracker Documentation',
+     [u'The Distro Tracker Developers'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -231,8 +231,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'PackageTrackingSystem', u'Package Tracking System Documentation',
-   u'Debian Package Tracking System Developers', 'PackageTrackingSystem', 'One line description of project.',
+  ('index', 'DistroTracker', u'Distro Tracker Documentation',
+   u'The Distro Tracker Developers', 'Distro Tracker', 'Synoptic of a Debian-based Distribution.',
    'Miscellaneous'),
 ]
 
--- pts/core/tests/tests_models.py
+++ pts/core/tests/tests_models.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2013 The Debian Package Tracking System Developers
+# Copyright 2013 The Distro Tracker Developers
 # See the COPYRIGHT file at the top-level directory of this distribution and
-# at http://deb.li/ptsauthors
+# at http://deb.li/DTAuthors
 #
-# This file is part of the Package Tracking System. It is subject to the
-# license terms in the LICENSE file found in the top-level directory of
-# this distribution and at http://deb.li/ptslicense. No part of the Package
-# Tracking System, including this file, may be copied, modified, propagated, or
-# distributed except according to the terms contained in the LICENSE file.
+# This file is part of Distro Tracker. It is subject to the license terms
+# in the LICENSE file found in the top-level directory of this
+# distribution and at http://deb.li/DTLicense. No part of Distro Tracker,
+# including this file, may be copied, modified, propagated, or distributed
+# except according to the terms contained in the LICENSE file.
 
 """
 Tests for the PTS core module's models.
