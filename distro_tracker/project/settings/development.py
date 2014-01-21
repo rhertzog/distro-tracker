86687f7b71c0305f7ccb05b819f18fa482d50af6
Raphaël Hertzog <hertzog@debian.org>
2014-01-21 10:18:27 +0100

    Re-enable debug_toolbar for development
    An updated version compatible with Django 1.6 is in Debian unstable.
    Drop the debug_toolbar settings and let it install its own
    default settings by default.

--- distro_tracker/project/settings/development.py
+++ distro_tracker/project/settings/development.py
@@ -28,28 +28,7 @@ TEMPLATE_LOADERS = (
 
 XHR_SIMULATED_DELAY = 0.5
 
-MIDDLEWARE_CLASSES += (
-#    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-
 INSTALLED_APPS += (
-#    'debug_toolbar',
+    'debug_toolbar',
 )
 
-DEBUG_TOOLBAR_CONFIG = {
-    'HIDE_DJANGO_SQL': True,
-    'INTERCEPT_REDIRECTS': False,
-    'SHOW_TEMPLATE_CONTEXT': False,
-}
-
-DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.version.VersionDebugPanel',
-    'debug_toolbar.panels.timer.TimerDebugPanel',
-    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-    'debug_toolbar.panels.headers.HeaderDebugPanel',
-    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-    'debug_toolbar.panels.sql.SQLDebugPanel',
-    'debug_toolbar.panels.logger.LoggingPanel',
-)
-
-DATABASE_ENGINE = 'dummy_for_debug_toolbar'
