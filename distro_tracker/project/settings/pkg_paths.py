94bdcbcf45b48fbcbb69896f3099c5aad5645eed
Raphaël Hertzog <hertzog@debian.org>
2014-07-10 18:29:21 +0200

    settings: fix path settings for the official package

--- distro_tracker/project/settings/pkg_paths.py
+++ distro_tracker/project/settings/pkg_paths.py
@@ -5,15 +5,6 @@ package.
 """
 from __future__ import unicode_literals
 
-import os.path
-
-DISTRO_TRACKER_BASE_PATH = '/var/lib/package-tracking-system'
-
-STATIC_ROOT = os.path.join(DISTRO_TRACKER_BASE_PATH, 'static')
-MEDIA_ROOT = os.path.join(DISTRO_TRACKER_BASE_PATH, 'media')
-TEMPLATE_DIRS = (
-    os.path.join(DISTRO_TRACKER_BASE_PATH, 'templates'),
-)
-
-DISTRO_TRACKER_KEYRING_DIRECTORY = os.path.join(DISTRO_TRACKER_BASE_PATH, 'keyring')
-DISTRO_TRACKER_CACHE_DIRECTORY = '/var/cache/package-tracking-system'
+DISTRO_TRACKER_DATA_PATH = '/var/lib/distro-tracker'
+DISTRO_TRACKER_CACHE_DIRECTORY = '/var/cache/distro-tracker'
+DISTRO_TRACKER_LOG_DIRECTORY = '/var/log/distro-tracker'
