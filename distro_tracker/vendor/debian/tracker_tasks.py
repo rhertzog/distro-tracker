rhertzog/distro-tracker

commit 0e516aae884321adf78f2e904dc0e4ff3f9defe9

vendor/debian: fix tests.UpdateExcusesTaskActionItemTest with Python 3
ERROR: test_action_item_updated (distro_tracker.vendor.debian.tests.UpdateExcusesTaskActionItemTest)
----------------------------------------------------------------------
Traceback (most recent call last):
  File "/home/rhertzog/deb/core/distro-tracker/distro_tracker/vendor/debian/tests.py", line 1696, in test_action_item_updated
    self.run_task()
  File "/home/rhertzog/deb/core/distro-tracker/distro_tracker/vendor/debian/tests.py", line 1594, in run_task
    self.task.execute()
  File "/home/rhertzog/deb/core/distro-tracker/distro_tracker/vendor/debian/tracker_tasks.py", line 944, in execute
    result = self._get_excuses_and_problems(content_lines)
  File "/home/rhertzog/deb/core/distro-tracker/distro_tracker/vendor/debian/tracker_tasks.py", line 837, in _get_excuses_and_problems
    line = line.decode('utf-8')
AttributeError: 'str' object has no attribute 'decode'

distro_tracker/vendor/debian/tracker_tasks.py
@@ -834,7 +834,6 @@ class UpdateExcusesTask(BaseTask):
         problematic = {}
         excuses = []
         for line in content_lines:
-            line = line.decode('utf-8')
             if '</ul>' in line:
                 # The inner list is closed -- all excuses for the package are
                 # processed and we're back to the top-level list.
@@ -934,7 +933,7 @@ class UpdateExcusesTask(BaseTask):
         if not updated:
             return
 
-        return response.iter_lines()
+        return response.iter_lines(decode_unicode=True)
 
     def execute(self):
         content_lines = self._get_update_excuses_content()

commit 98dd48b07df4ef5b820f9e2358e1181b0abfe50e

vendor/debian: fix UpdateExcusesTask
It looks like that requests' iter_lines(decode_unicode=True) doesn't work
as expected with version 2.0 deployed on tracker.debian.org and doesn't
always return unicode strings. So we bring back the manual decode in the
case where we get a binary type.

Regression introduced in 0e516aae884321adf78f2e904dc0e4ff3f9defe9.

The failure was this one:

Traceback (most recent call last):
  File "/srv/tracker.debian.org/distro-tracker/distro_tracker/core/tasks.py", line 518, in run
    task.execute()
  File "/srv/tracker.debian.org/distro-tracker/distro_tracker/vendor/debian/tracker_tasks.py", line 951, in execute
    result = self._get_excuses_and_problems(content_lines)
  File "/srv/tracker.debian.org/distro-tracker/distro_tracker/vendor/debian/tracker_tasks.py", line 859, in _get_excuses_and_problems
    if '</ul>' in line:
UnicodeDecodeError: 'ascii' codec can't decode byte 0xc5 in position 19: ordinal not in range(128)

distro_tracker/vendor/debian/tracker_tasks.py
@@ -856,6 +856,8 @@ class UpdateExcusesTask(BaseTask):
         problematic = {}
         excuses = []
         for line in content_lines:
+            if isinstance(line, six.binary_type):
+                line = line.decode('utf-8')
             if '</ul>' in line:
                 # The inner list is closed -- all excuses for the package are
                 # processed and we're back to the top-level list.