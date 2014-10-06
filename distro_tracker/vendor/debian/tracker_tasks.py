rhertzog/distro-tracker

commit a4298091ca9e5970c79e0fad048a3c0779fcf986

Correct URLs to ci.debian.net again (See: #762716)

distro_tracker/vendor/debian/tracker_tasks.py
@@ -2095,7 +2095,7 @@ class UpdateDebciStatusTask(BaseTask):
             log_dir = package_name[:4]
         else:
             log_dir = package_name[:1]
-        url = 'http://ci.debian.net/' + log_dir + '/' + package_name
+        url = 'http://ci.debian.net/packages/' + log_dir + '/' + package_name + '/'
         log = 'http://ci.debian.net/data/packages/unstable/amd64/' + \
             log_dir + "/" + package_name + '/latest-autopkgtest/log'
         debci_action_item.short_description = self.ITEM_DESCRIPTION.format(

distro_tracker/vendor/debian/tests.py
@@ -4885,7 +4885,7 @@ class UpdateDebciStatusTaskTest(TestCase):
         # Check that the ActionItem contains the correct contents.
         self.assertEqual(self.other_package.action_items.count(), 1)
         action_item = self.other_package.action_items.all()[0]
-        url = "http://ci.debian.net/o/other-package"
+        url = "http://ci.debian.net/packages/o/other-package/"
         log = "http://ci.debian.net/data/packages/unstable/amd64/o/" + \
             "other-package/latest-autopkgtest/log"
         self.assertIn(url, action_item.short_description)

commit 3f29f273e7326fa23d3524dd68a99a4079dac393

vendor/debian: fix flake8 error
Introduced by a4298091ca9e5970c79e0fad048a3c0779fcf986.

distro_tracker/vendor/debian/tracker_tasks.py
@@ -2095,7 +2095,8 @@ class UpdateDebciStatusTask(BaseTask):
             log_dir = package_name[:4]
         else:
             log_dir = package_name[:1]
-        url = 'http://ci.debian.net/packages/' + log_dir + '/' + package_name + '/'
+        url = 'http://ci.debian.net/packages/' + log_dir + '/' + \
+            package_name + '/'
         log = 'http://ci.debian.net/data/packages/unstable/amd64/' + \
             log_dir + "/" + package_name + '/latest-autopkgtest/log'
         debci_action_item.short_description = self.ITEM_DESCRIPTION.format(