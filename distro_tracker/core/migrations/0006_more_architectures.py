2e9e9e05e88ca168944d9e9a66f259eeabe4e794
Raphaël Hertzog <hertzog@debian.org>
2015-12-10 21:31:09 +0100

    core: add all Linux architectures known to dpkg-architecture
    Closes: #793547

---
+++ distro_tracker/core/migrations/0006_more_architectures.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def forwards_func(apps, schema_editor):
+    # We get the model from the versioned app registry;
+    # if we directly import it, it'll be the wrong version
+    Architecture = apps.get_model('core', 'Architecture')
+    db_alias = schema_editor.connection.alias
+    Architecture.objects.using(db_alias).bulk_create([
+        Architecture(name='alpha'),
+        Architecture(name='arm'),
+        Architecture(name='arm64'),
+        Architecture(name='armeb'),
+        Architecture(name='avr32'),
+        Architecture(name='hppa'),
+        Architecture(name='m32r'),
+        Architecture(name='m68k'),
+        Architecture(name='mips64'),
+        Architecture(name='mips64el'),
+        Architecture(name='mipsn32'),
+        Architecture(name='mipsn32el'),
+        Architecture(name='or1k'),
+        Architecture(name='powerpcel'),
+        Architecture(name='powerpcspe'),
+        Architecture(name='ppc64'),
+        Architecture(name='ppc64el'),
+        Architecture(name='sh3'),
+        Architecture(name='sh3eb'),
+        Architecture(name='sh4'),
+        Architecture(name='sh4eb'),
+        Architecture(name='sparc64'),
+        Architecture(name='x32'),
+    ])
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0005_uri_as_char_field'),
+    ]
+
+    operations = [
+        migrations.RunPython(
+            forwards_func,
+        ),
+    ]
