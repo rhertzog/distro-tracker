rhertzog/distro-tracker

commit dac264a2caabbd45f7a483a2c678bad443eb6162

settings: debug_toolbar needs a MIDDLEWARE setting too

distro_tracker/project/settings/development.py
@@ -4,7 +4,7 @@ Appropriate settings to run during development.
 When running in development mode, selected.py should point to this file.
 """
 
-from .defaults import INSTALLED_APPS, TEMPLATES
+from .defaults import INSTALLED_APPS, MIDDLEWARE, TEMPLATES
 from .db_sqlite import DATABASES  # noqa
 
 DEBUG = True
@@ -31,3 +31,5 @@ XHR_SIMULATED_DELAY = 0.5
 INSTALLED_APPS += (
     'debug_toolbar',
 )
+
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')

commit b1ac88152ef9514b64f16a600e4e8f4d0c967662

settings: use lists instead of tuples for many settings
All the modified settings default to lists in the current version of
Django. Special care must be taken to ensure that we duplicate the list
objects in the various namespaces otherwise we get a nice mess with a
single shared configuration combining all the operations of all the
settings that are loaded but not necessarily imported in the main settings
namespace.

We add __all__ attributes to some of the core modules to better control
what settings are actually exported.

This also fixes the regression introduced in
dac264a2caabbd45f7a483a2c678bad443eb6162 where the debug_toolbar
application suddenly started to be required for tests too.

distro_tracker/project/settings/debian.py
@@ -1,18 +1,36 @@
 """Debian.org specific settings"""
 
 import os.path
-from .defaults import (
-    INSTALLED_APPS, MIDDLEWARE, AUTHENTICATION_BACKENDS)
+
+from . import defaults
 from .db_postgresql import DATABASES  # noqa
 
-INSTALLED_APPS += (
+__all__ = [
+    'ALLOWED_HOSTS',
+    'AUTHENTICATION_BACKENDS',
+    'DATABASES',
+    'DISTRO_TRACKER_BUGS_PANEL_TEMPLATE',
+    'DISTRO_TRACKER_CVE_URL',
+    'DISTRO_TRACKER_DEBIAN_PIUPARTS_SUITES',
+    'DISTRO_TRACKER_DEVEL_REPOSITORIES',
+    'DISTRO_TRACKER_FQDN',
+    'DISTRO_TRACKER_REMOVALS_URL',
+    'DISTRO_TRACKER_VENDOR_RULES',
+    'DJANGO_EMAIL_ACCOUNTS_POST_LOGOUT_REDIRECT',
+    'DJANGO_EMAIL_ACCOUNTS_PRE_LOGIN_HOOK',
+    'INSTALLED_APPS',
+    'MIDDLEWARE',
+]
+
+INSTALLED_APPS = defaults.INSTALLED_APPS.copy()
+INSTALLED_APPS.extend([
     # Many debian.org customizations
     'distro_tracker.vendor.debian',
     # Generate warnings for outdated values of the Standards-Version field
     'distro_tracker.stdver_warnings',
     # Extract common files from the source package
     'distro_tracker.extract_source_files',
-)
+])
 
 # Official service name
 DISTRO_TRACKER_FQDN = "tracker.debian.org"
@@ -25,9 +43,11 @@ ALLOWED_HOSTS = [
 # those settings in a development environment too).
 if os.path.isdir('/srv/tracker.debian.org/data'):
     DISTRO_TRACKER_DATA_PATH = '/srv/tracker.debian.org/data'
+    __all__.append('DISTRO_TRACKER_DATA_PATH')
 
 if os.path.isfile('/etc/ssl/ca-global/ca-certificates.crt'):
     DISTRO_TRACKER_CA_BUNDLE = '/etc/ssl/ca-global/ca-certificates.crt'
+    __all__.append('DISTRO_TRACKER_CA_BUNDLE')
 
 #: A module implementing vendor-specific hooks for use by Distro Tracker.
 #: For more information see :py:mod:`distro_tracker.vendor`.
@@ -51,14 +71,15 @@ DISTRO_TRACKER_DEVEL_REPOSITORIES = ['unstable', 'experimental']
 DISTRO_TRACKER_CVE_URL = 'https://security-tracker.debian.org/tracker/'
 
 # Various settings for sso.debian.org support
-_index_auth = MIDDLEWARE.index(
-    'django.contrib.auth.middleware.AuthenticationMiddleware') + 1
-MIDDLEWARE = MIDDLEWARE[:_index_auth] + \
-    ['distro_tracker.vendor.debian.sso_auth.DebianSsoUserMiddleware'] + \
-    MIDDLEWARE[_index_auth:]
-AUTHENTICATION_BACKENDS = \
-    ('distro_tracker.vendor.debian.sso_auth.DebianSsoUserBackend',) + \
-    AUTHENTICATION_BACKENDS
+MIDDLEWARE = defaults.MIDDLEWARE.copy()
+MIDDLEWARE.insert(
+    MIDDLEWARE.index(
+        'django.contrib.auth.middleware.AuthenticationMiddleware') + 1,
+    'distro_tracker.vendor.debian.sso_auth.DebianSsoUserMiddleware'
+)
+AUTHENTICATION_BACKENDS = defaults.AUTHENTICATION_BACKENDS.copy()
+AUTHENTICATION_BACKENDS.insert(
+    0, 'distro_tracker.vendor.debian.sso_auth.DebianSsoUserBackend')
 DJANGO_EMAIL_ACCOUNTS_PRE_LOGIN_HOOK = \
     'distro_tracker.vendor.debian.rules.pre_login'
 DJANGO_EMAIL_ACCOUNTS_POST_LOGOUT_REDIRECT = \

distro_tracker/project/settings/defaults.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2017 The Distro Tracker Developers
+# Copyright 2013-2018 The Distro Tracker Developers
 # See the COPYRIGHT file at the top-level directory of this distribution and
 # at https://deb.li/DTAuthors
 #
@@ -168,19 +168,19 @@ MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-)
+]
 
 # List of finder classes that know how to find static files in
 # various locations.
-STATICFILES_FINDERS = (
+STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
     # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
-)
+]
 
 # Make this unique, and don't share it with anybody.
 try:
@@ -226,9 +226,9 @@ MIDDLEWARE = [
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-AUTHENTICATION_BACKENDS = (
+AUTHENTICATION_BACKENDS = [
     'django_email_accounts.auth.UserEmailBackend',
-)
+]
 
 AUTH_USER_MODEL = 'accounts.User'
 
@@ -237,7 +237,7 @@ ROOT_URLCONF = 'distro_tracker.project.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'distro_tracker.project.wsgi.application'
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -249,7 +249,7 @@ INSTALLED_APPS = (
     'distro_tracker.core',
     'distro_tracker.accounts',
     'distro_tracker.mail',
-)
+]
 
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.

distro_tracker/project/settings/development.py
@@ -4,8 +4,21 @@ Appropriate settings to run during development.
 When running in development mode, selected.py should point to this file.
 """
 
-from .defaults import INSTALLED_APPS, MIDDLEWARE, TEMPLATES
+from . import defaults
 from .db_sqlite import DATABASES  # noqa
+
+__all__ = [
+    'ADMINS',
+    'CACHES',
+    'DATABASES',
+    'DEBUG',
+    'EMAIL_BACKEND',
+    'INSTALLED_APPS',
+    'MIDDLEWARE',
+    'SITE_URL',
+    'TEMPLATES',
+    'XHR_SIMULATED_DELAY',
+]
 
 DEBUG = True
 
@@ -21,6 +34,10 @@ CACHES = {
     }
 }
 
+TEMPLATES = defaults.TEMPLATES.copy()
+TEMPLATES[0] = TEMPLATES[0].copy()
+TEMPLATES[0]['OPTIONS'] = TEMPLATES[0]['OPTIONS'].copy()
+TEMPLATES[0]['OPTIONS']['loaders'] = TEMPLATES[0]['OPTIONS']['loaders'].copy()
 TEMPLATES[0]['OPTIONS']['loaders'] = [
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
@@ -28,8 +45,8 @@ TEMPLATES[0]['OPTIONS']['loaders'] = [
 
 XHR_SIMULATED_DELAY = 0.5
 
-INSTALLED_APPS += (
-    'debug_toolbar',
-)
+INSTALLED_APPS = defaults.INSTALLED_APPS.copy()
+INSTALLED_APPS.append('debug_toolbar')
 
+MIDDLEWARE = defaults.MIDDLEWARE.copy()
 MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')

distro_tracker/project/settings/kali.py
@@ -1,9 +1,21 @@
 """Kali.org specific settings"""
 
-from .defaults import INSTALLED_APPS
+from . import defaults
 from .db_postgresql import DATABASES  # noqa
 
-INSTALLED_APPS += (
+__all__ = [
+    'DATABASES',
+    'DISTRO_TRACKER_DEVEL_REPOSITORIES',
+    'DISTRO_TRACKER_FQDN',
+    'DISTRO_TRACKER_VENDOR_NAME',
+    'DISTRO_TRACKER_VENDOR_RULES',
+    'DISTRO_TRACKER_VENDOR_URL',
+    'DJANGO_EMAIL_ACCOUNTS_USE_CAPTCHA',
+    'INSTALLED_APPS',
+]
+
+INSTALLED_APPS = defaults.INSTALLED_APPS.copy()
+INSTALLED_APPS.extend([
     # Extract common files from the source package
     'distro_tracker.extract_source_files',
 
@@ -12,7 +24,7 @@ INSTALLED_APPS += (
 
     # Captcha support
     'captcha',
-)
+])
 
 # Official service name
 DISTRO_TRACKER_FQDN = "pkg.kali.org"

distro_tracker/project/settings/local.py.sample
@@ -8,13 +8,13 @@ overrides on top of those type-of-installation-specific settings.
 """
 # Load the selected configuration (selected.py is a symlink to preferred config)
 
-from .defaults import INSTALLED_APPS
-from .selected import *  # noqa
+from . imports defaults
+from .selected import *
 
 # If you want to run a development setup close to what's running on
 # tracker.debian.org, then uncomment the next two lines.
-# from .debian import *             # noqa
-# from .db_sqlite import DATABASES  # noqa
+# from .debian import *
+# from .db_sqlite import DATABASES
 
 ## Add your custom settings here
 
@@ -30,7 +30,8 @@ from .selected import *  # noqa
 
 # You can enable supplementary Django applications here, refer to the
 # documation for details about what they do
-INSTALLED_APPS += (
+INSTALLED_APPS = defaults.INSTALLED_APPS.copy()
+INSTALLED_APPS.extend([
     # Can be useful to create visualizations of the models
     # 'django_extensions',
 
@@ -45,4 +46,4 @@ INSTALLED_APPS += (
 
     # Must be added if you set DJANGO_EMAIL_ACCOUNTS_USE_CAPTCHA to True
     # 'captcha',
-)
+])

distro_tracker/project/settings/pkg_paths.py
@@ -4,6 +4,12 @@ The paths indicated in this file are those setup by the official Debian
 package.
 """
 
+__all__ = [
+    'DISTRO_TRACKER_DATA_PATH',
+    'DISTRO_TRACKER_CACHE_DIRECTORY',
+    'DISTRO_TRACKER_LOG_DIRECTORY',
+]
+
 DISTRO_TRACKER_DATA_PATH = '/var/lib/distro-tracker'
 DISTRO_TRACKER_CACHE_DIRECTORY = '/var/cache/distro-tracker'
 DISTRO_TRACKER_LOG_DIRECTORY = '/var/log/distro-tracker'

distro_tracker/project/settings/production.py
@@ -5,7 +5,7 @@ When running in production, selected.py should point to this file.
 """
 
 # PostgreSQL should be used in production
-from .db_postgresql import *  # noqa
+from .db_postgresql import DATABASES  # noqa
 
 # Use paths from the package
-from .pkg_paths import *      # noqa
+from .pkg_paths import *  # noqa

distro_tracker/project/settings/test.py
@@ -1,19 +1,23 @@
 """Appropriate settings to run the test suite."""
 
+from . import defaults
 from .development import *  # noqa
-from .defaults import INSTALLED_APPS
 
 # Don't use bcrypt to run tests (speed gain)
-PASSWORD_HASHERS = (
+PASSWORD_HASHERS = [
     'django.contrib.auth.hashers.SHA1PasswordHasher',
-)
+]
+
+# Restore INSTALLED_APPS and MIDDLEWARE from defaults to disable debug_toolbar
+INSTALLED_APPS = defaults.INSTALLED_APPS.copy()
+MIDDLEWARE = defaults.MIDDLEWARE
 
 # When running the test suite, enable all apps so that we have all the models
-INSTALLED_APPS += (
+INSTALLED_APPS.extend([
     'distro_tracker.auto_news',
     'distro_tracker.derivative',
     'distro_tracker.extract_source_files',
     'distro_tracker.stdver_warnings',
     'distro_tracker.vendor',
     'distro_tracker.vendor.debian',
-)
+])