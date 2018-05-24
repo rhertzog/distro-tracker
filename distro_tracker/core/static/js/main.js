8b435416c96c78b62b6e1c874cb7c1bd4c0a03bf
Arthur Del Esposte <arthurmde@gmail.com>
2018-05-24 16:29:35 -0300

    Add Popper.js to add popovers with more details to package tables

--- distro_tracker/core/static/js/main.js
+++ distro_tracker/core/static/js/main.js
@@ -213,4 +213,23 @@ $(function() {
         $(this).closest('form').submit();
         return false;
     });
+
+  $(".popover-hover").popover({
+    trigger: "manual",
+    html: true,
+    animation: true
+  }).on("mouseenter", function () {
+    var _this = this;
+    $(this).popover("show");
+    $(".popover").on("mouseleave", function () {
+      $(_this).popover('hide');
+    });
+  }).on("mouseleave", function () {
+    var _this = this;
+    setTimeout(function () {
+      if (!$(".popover:hover").length) {
+        $(_this).popover("hide");
+      }
+    }, 100);
+  });
 });
